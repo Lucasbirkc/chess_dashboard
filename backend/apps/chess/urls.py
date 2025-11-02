db570a57c8ccb72e9d1cdbe8e999900da7ccdb93
Lucasbirkc <Lucasbirkc@gmail.com>
2025-11-02 09:00:20 +0100

    Setup initial chess urls

---
+++ backend/apps/chess/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('players/win_rate/<str:username>/', views.PlayerWinRateView.as_view(), name='player-win-rate'),
+    path('players/peak_rating/<str:username>/', views.PlayerPeakRatingView.as_view(), name='player-peak-rating'),
+    path('players/latest_rating/<str:username>', views.PlayerLatestRatingView.as_view(), name='player-latest-rating'),
+    path('players/total_games/<str:username>', views.PlayerTotalGamesView.as_view(), name='player-total-games'),
+    path('players/openings/<str:username>/', views.PlayerOpeningStatsView.as_view(), name='player-opening-stats'),
+    
+    path('players/analytics/<str:username>/', views.PlayerAnalyticsOverview.as_view(), name='player-analytics'),
+]
